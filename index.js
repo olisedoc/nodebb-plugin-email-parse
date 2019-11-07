7ba32aa0c49c3af90d5d8874acc35961bb1a8300
Oliver Sedoc <osedoc@gmail.com>
2019-11-08 00:01:14 +0100

    Check if post already have been parsed with regex

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ var EmailParse = {
 	emojiRegex : {
 		regex: /(:[\w_-]+: ?)/g
 	},
+
+	htmlRegex : {
+		regex: /<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>/g
+	},
+
+
     
     init: function () {
 		// Load saved config
@@ -53,8 +59,11 @@ var EmailParse = {
     parsePost: function (data, callback) {
 		async.waterfall([
 			function (next) {
-				if (data && data.params && data.params.notification && parser) {
-					data.params.body = parser.render(data.params.body);
+				if (data && data.params && data.params.notification && parser && !isHtmlParsed) {
+					var isHtmlParsed = EmailParse.htmlRegex.regex.test(data.params.body);
+					if (!isHtmlParsed) {
+						data.params.body = parser.render(data.params.body);
+					};
 				};
 				next(null, data);
 			},
